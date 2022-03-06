4dfd3d1ffc5a011cc37a2e965c889b589457ecf2
Pasha0909 <73825639+Pasha0909@users.noreply.github.com>
2022-03-06 11:56:33 +0500

    module1-task3-test2

--- src/main.js
+++ src/main.js
@@ -3,17 +3,20 @@ import {renderTemplate, RenderPosition} from './render.js';
 import {createSiteTabTemplate} from './view/site-tab-view.js';
 import {createSiteFilterTeamplate} from './view/site-filter-view.js';
 import {createSiteSortTeamplate} from './view/site-sort-view.js';
-//import {createSiteListTeamplate} from './view/site-list-view.js';
-import {createSiteEventEditTeamplate} from './view/site-event-edit-view.js';
+//import {createSiteEditFormTeamplate} from './view/site-edit-form-view.js';
+//import {createSiteAddPointTeamplate} from './view/site-add-point-view.js';
 
 const tripTab = document.querySelector('.trip-main');
 const tripFilters = document.querySelector('.trip-controls__filters');
 const tripEventsSort = document.querySelector('.trip-events');
-//const tripEventsList = document.querySelector('.trip-events__item');
-const tripEventsEdit = document.querySelector('.trip-events__list');
+//const tr = document.querySelector('.trip-events');
 
 renderTemplate(tripTab,createSiteTabTemplate(), RenderPosition.AFTERBEGIN);
 renderTemplate(tripFilters, createSiteFilterTeamplate(), RenderPosition.BEFOREBEGIN);
-renderTemplate(tripEventsSort, createSiteSortTeamplate(), RenderPosition.AFTERBEGIN);
-//renderTemplate(tripEventsList, createSiteListTeamplate(), RenderPosition.AFTEREND);
-renderTemplate(tripEventsEdit, createSiteEventEditTeamplate(), RenderPosition.BEFOREEND);
+renderTemplate(tripEventsSort, createSiteSortTeamplate(), RenderPosition.BEFOREBEGIN);
+//renderTemplate(tr, createSiteEditFormTeamplate(), RenderPosition.AFTERBEGIN);
+
+//for (let i = 0; i < 3; i++){
+//renderTemplate(tripEventsSort, createSiteAddPointTeamplate(), RenderPosition.AFTEREND);
+//}
+
